9ab31e19605f0aee1c61e72bfe5541e567ec5796
deepakgawade <deepakgawade22@gmail.com>
2022-04-23 23:52:54 +0530

    add link list

---
+++ linklist-1.py
@@ -0,0 +1,39 @@
+from webob import second
+
+
+class Node:
+ #create a single node
+    def  __init__(self,data):
+        self.data=data
+        self.next=None
+        
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    
+    #method to print linkList
+    def printList(self):
+        temp=self.head
+        while(temp):
+
+            print(temp.data)
+            temp=temp.next
+
+    
+
+#main code start here
+
+if __name__=='__main__':
+
+
+    #create empty List
+    llist=LinkedList()
+    llist.head=Node(1)
+    second=Node(2)
+    third=Node(3)
+
+    #link first node with second
+    llist.head.next=second
+    second.next=third
+
+    llist.printList()
