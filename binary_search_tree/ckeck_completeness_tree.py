9ec481a78c69bc0253ff4f193d4cd90dea89d07b
deepakgawade <deepakgawade22@gmail.com>
2022-10-04 11:05:20 +0530

    check tree completness

---
+++ binary_search_tree/ckeck_completeness_tree.py
@@ -0,0 +1,21 @@
+#958. Check Completeness of a Binary Tree
+#create queue. append a node. check node is null and return 
+#move from left to right till reach th end and after end theri shuld not be any node.
+#this only works if we move from left -> right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+if __name__=="__main__":
+    def isCompleteTree(self, root):
+        queuq=[root]
+        i=0
+        while queuq[i]:
+            queuq.append(queuq[i].left)
+            queuq.append(queuq[i].right)
+            i=i+1
+        return not any(queuq[i:])
+
+    #create a tree using tupple and test the solution for completeness.
