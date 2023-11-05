0ae2d39cef35bfa4150065e155a79b883d07a8dd
Neosoft <neosoft@NTIPLs-MacBook-Pro.local>
2023-11-05 16:37:47 +0530

    added repeat abc

---
+++ pattern/pattern8.py
@@ -0,0 +1,5 @@
+for i in range(6):
+    strs=""
+    for j in range(5-i):
+        strs=strs+chr(65+j)
+    print(strs)
---
+++ pattern/pattern9.py
@@ -0,0 +1,6 @@
+for i in range(6):
+    strs=""
+    for j in range(0,i):
+        strs=strs+chr(64+i)
+
+    print(strs)
