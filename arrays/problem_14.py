388de5424075e5b3dc2fc4dbb3894621f7318653
deepakgawade <deepakgawade22@gmail.com>
2024-01-09 17:55:36 +0530

    added moores algo

---
+++ arrays/problem_14.py
@@ -0,0 +1,18 @@
+def mooreAlgo(arr):
+    n=len(arr)
+    count=0
+    element=None
+
+    for i in range(n):
+
+        if count==0:
+            count=1
+            element=arr[i]
+
+        elif element==arr[i]:
+            count+=1
+        else:
+            count-=1
+    if count>(n/2):
+        return element
+    
