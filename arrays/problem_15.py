2aee12d3a20930e818d2ff6ef09d3acd3cb24e5b
deepakgawade <deepakgawade22@gmail.com>
2024-01-10 14:22:50 +0530

    added kadens algorithm

--- arrays/problem_15.py
+++ arrays/problem_15.py
@@ -5,8 +5,25 @@ def kadensAlgo(arr):
     sum=0
     max= -sys.maxsize-1
 
-    #start=0
-
+    start=0
+    ansStart, ansEnd= -1,-1
     for i in range(n):
+        if sum==0:
+            start=i
+        
+        sum+=arr[i]
+
+        if sum>max:
+            max=sum
+            ansStart=start
+            ansEnd=i
+
+        if sum<0:
+            sum=0
+    for i in range(ansStart,ansEnd+1):
+        print(arr[i])
+
+    return max
+        
         
 
