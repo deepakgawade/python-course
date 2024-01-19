5324b0159e3359166215e0e024d5e861d503a5c6
deepakgawade <deepakgawade22@gmail.com>
2024-01-19 22:01:20 +0530

    added better approach to metrix

--- arrays/problem_21.py
+++ arrays/problem_21.py
@@ -30,13 +30,34 @@ def zeroMatrix(matrix, n,m):
 
     return matrix  
 
+def betterZeroMatrix(matrix,n,m):
+    row=[0]*n
+    col=[0]*m
+
+    for i in range(n):
+        for j in range(m):
+
+            if matrix[i][j]==0:#check zero makr -1
+                row[i]=1
+                col[j]=1
+
+    for i in range(n):
+        for j in range(m):
+
+            if col[j] or row[i]:#check zero makr -1
+               matrix[i][j]=0
+    return matrix
+               
+
+
     
 if __name__=="__main__":
     matrix= [[1,1,1],[1,0,1],[1,1,1]]
     n=len(matrix)
     m=len(matrix[0])
 
-    ans = zeroMatrix(matrix, n,m)
+    #ans = zeroMatrix(matrix, n,m)
+    ans =betterZeroMatrix(matrix,n,m)
 
     for row in ans:
         for ele in row:
