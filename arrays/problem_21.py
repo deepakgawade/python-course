9396ad3d1cc3b17823ca988b181309ba843665ad
deepakgawade <deepakgawade22@gmail.com>
2024-01-28 10:42:11 +0530

    added optimal approach for matrix

--- arrays/problem_21.py
+++ arrays/problem_21.py
@@ -49,6 +49,51 @@ def betterZeroMatrix(matrix,n,m):
     return matrix
                
 
+def optimalZeroMatrix(matrix,n,m):
+    # space complexity O(1)
+    # time complexity 2O(n*m)
+    # row=[0]*n-. matrix[..][0]
+
+    # col=[0]*m->matrix[0][..]
+    col0=1
+
+    for i in range(n):
+        for j in range(m):
+
+            if matrix[i][j]==0:#check zero makr -1
+                # row[i]=1
+                # col[j]=1
+
+                #mark ith row
+                matrix[i][0]=0
+
+                #mark jth col
+                if j!=0:
+                    
+                    matrix[0][j]=0
+                else:
+                    col0=0
+    
+    for i in range(1,n):
+        for j in range(1,m):
+
+            if matrix[i][j]!=0:#check zero makr -1
+               #check for row and column if it is zero
+               if matrix[i][0]==0 or matrix[0][j]==0:
+                   matrix[i][j]=0   
+
+                
+                    
+    if matrix[0][0]==0:
+
+        for j in range(n): 
+                matrix[0][j]=0
+    if col0==0:
+            for i in range(n):
+                matrix[i][0]=0 
+
+    return matrix
+         
 
     
 if __name__=="__main__":
@@ -57,7 +102,8 @@ if __name__=="__main__":
     m=len(matrix[0])
 
     #ans = zeroMatrix(matrix, n,m)
-    ans =betterZeroMatrix(matrix,n,m)
+    #ans =betterZeroMatrix(matrix,n,m)
+    ans = optimalZeroMatrix(matrix,n,m)
 
     for row in ans:
         for ele in row:
