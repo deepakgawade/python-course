c0376e1fa290d57a8061d7438a22791c46a72290
deepakgawade <deepakgawade22@gmail.com>
2024-02-04 23:13:21 +0530

    added optimal triplet

--- arrays/problem_27.py
+++ arrays/problem_27.py
@@ -20,6 +20,61 @@ def sumOfTiplet(arr,x):
 
     return ans
 
+def betterSumTriplet(arr,x):
+    n=len(arr)
+    answer=[]
+    sst2=set()
+    
+
+    for i in range(n):
+        stt=set()
+        for j in range(i+1,n):
+
+            num= -(arr[i]+arr[j])
+            if num in stt:
+                ans=[arr[i],arr[j],num]
+                ans.sort()
+                sst2.add(tuple(ans))
+            stt.add(arr[j])
+            
+    fromset=[list(item) for item in sst2]
+    return fromset
+
+    #O(n2)+O(nlogn)
+    #O(N)+ 2*O(triplets)
+
+
+def optimalTriplet(arr,x):
+    n=len(arr)
+    arr.sort()
+    answer=[]
+    for i in range(n):
+        if i>0 and arr[i]==arr[i-1]:
+            continue
+        j=i+1
+        k=n-1
+
+        while j<k :
+            sum= arr[i]+arr[j]+arr[k]
+            if sum<x:
+                j+=1
+
+            elif sum> x:
+                k-=1
+
+            else:
+
+                ans= [arr[i],arr[j], arr[k]]
+                answer.append(ans) 
+                j+=1
+                k-=1
+                while j<k and  arr[j]==arr[j-1]:
+                    j+=1
+                while j<k and arr[k]==arr[k+ 1]: 
+                    k-=1
+
+    return answer
+                 
 if __name__=="__main__":
     nums=[-1,0,1,2,-1,-4]
-    print(sumOfTiplet(nums,0))
+    print(optimalTriplet(nums,0))
