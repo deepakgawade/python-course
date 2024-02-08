df295cd7f95d562a2265484e9d9ecc6d667303be
deepakgawade <deepakgawade22@gmail.com>
2024-02-08 22:38:34 +0530

    kadanes algorithm

--- arrays/problem_29.py
+++ arrays/problem_29.py
@@ -11,9 +11,31 @@ def betterApproch(arr):
 
     return maxx
 
+
+def optimalApproach(arr):
+    #kadane's algorithm
+    n=len(arr)
+    mpp={}
+    maxi=0
+    sum=0
+    for i in range(n):
+        sum+=arr[i]
+        if sum==0:
+          maxi = i+1
+        else:
+            if sum in mpp:
+                maxi=max(maxi, i - mpp[sum])
+            else:
+                mpp[sum]=i
+    return maxi
+
+
+
+
 if __name__=="__main__":
    elelmsy= [9, -3, 3, -1, 6, -5]
 
    print(betterApproch(elelmsy))
+   print(optimalApproach(elelmsy))
 
  
