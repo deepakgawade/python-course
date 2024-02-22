5118e477c67428a57a66c39d54757d7d9db892b4
deepakgawade <deepakgawade22@gmail.com>
2024-02-22 22:16:00 +0530

    upper bound

---
+++ binary_search/problem_6.py
@@ -0,0 +1,25 @@
+def  upperBound(arr, target):
+    n=len(arr)
+    low=0
+    high=n-1
+    ans=target
+    while low<=high:
+
+        mid = (low+high)//2
+
+   
+        if arr[mid] > target:
+        
+            ans= arr[mid]
+            high=mid-1 
+        else:
+            low=mid+1
+    return ans
+    #TC log(n)
+    #SC O(1)
+if __name__=="__main__":
+    elements=[3,5,8,15,19]
+    elements2=[1,2,2,3,8,9,12,34,77,89,90]
+    target=9
+    sum=upperBound(elements2,100)
+    print([i for i in sum])
